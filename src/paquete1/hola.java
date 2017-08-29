6da76fef560c82f5d64ad40b1df272c6953ceab2
Alumno <Alumno@ws204-03>
2017-08-29 15:33:39 -0700

    v1.0

---
+++ src/paquete1/hola.java
@@ -0,0 +1,16 @@
+package paquete1;
+
+public class hola {
+
+	public hola() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hola!....");
+
+	}
+
+}
